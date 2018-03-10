9d4b53623f480825e72205417131abfa46677730
rileyp2000 <rileyp@bishopireton.org>
2018-03-10 17:05:23 -0500

    organized classes

--- HackTJ/src/PathFinder.java
+++ HackTJ/src/PathFinder.java
@@ -7,76 +7,11 @@ import java.util.Scanner;
 public class PathFinder {
 	int[][] grid;
 
-	public PathFinder() {
-		Scanner keyboard = new Scanner(System.in);
-		String fileName;
-
-		// Open input file:
-
-		System.out.print("\nEnter input file name: ");
-		fileName = keyboard.nextLine().trim();
-
-		Scanner sc = fileReader(fileName);
-		grid = createGrid(sc);
-		System.out.println("max cookies: " + optimalPathFromTop(0,0,grid.length-1, grid[0].length-1));
+	public PathFinder(Maze m) {
+		grid = m.getMaze();
+		//System.out.println("max cookies: " + optimalPathFromTop(0,0,grid.length-1, grid[0].length-1));
 	}
 
-	/**
-	 * reads in a file
-	 * 
-	 * @param s
-	 *            file name
-	 * @return scanner representing the given file
-	 */
-	public Scanner fileReader(String s) {
-		File f = new File(s);
-		Scanner daGrid = null;
-
-		try {
-			daGrid = new Scanner(f);
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found!");
-			System.exit(0);
-		}
-
-		return daGrid;
-	}
-
-	/**
-	 * This takes in a scanner and then converts into an int[][]
-	 * 
-	 * @param fileGrid
-	 *            the scanner containing the grid
-	 * @return a 2D array of ints that represents the maze
-	 */
-	public int[][] createGrid(Scanner fileGrid) {
-		String line;
-
-		ArrayList<int[]> temp = new ArrayList<int[]>();
-		while (fileGrid.hasNextLine()) {
-
-			line = fileGrid.nextLine();
-			// String[] unparsed = line.split(" ");
-			ArrayList<String> unparsed = new ArrayList<String>(Arrays.asList(line.split(" ")));
-			for (int i = unparsed.size() - 1; i >= 0; i--) {
-				if (unparsed.get(i).equals(""))
-					unparsed.remove(i);
-			}
-
-			int[] row = new int[unparsed.size()];
-
-			for (int i = 0; i < unparsed.size(); i++) {
-				// System.out.println(unparsed.get(i));
-				row[i] = Integer.parseInt(unparsed.get(i));
-			}
-
-			temp.add(row);
-		}
-		int[][] ret = new int[temp.size()][temp.get(0).length];
-		for (int i = 0; i < temp.size(); i++)
-			ret[i] = temp.get(i);
-		return ret;
-	}
 
 	public int lookUp(int r, int c) {
 		try {
