20221d37657c198aa10e21ffb1edd2aca227950a
Matthew Grillo <grillom@bishopireton.org>
2018-03-11 08:31:49 -0400

    tt

--- HackTJ/src/Deck.java
+++ HackTJ/src/Deck.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck extends ArrayList<Card>{
+	ArrayList<Card> missing = new ArrayList<Card>();
 	public Deck(){
 		for (Suit suit: Suit.values()){
 			for (int i = 1; i < 14; i++){
@@ -14,10 +15,22 @@ public class Deck extends ArrayList<Card>{
 	public Deck(boolean b){
 		super();
 	}
+	public ArrayList<Card> getMissing(){
+		return missing;
+	}
 	public void draw(int num, ArrayList<Card> hand){
 		for (int i = 0; i < num; i++){
 			hand.add(this.get(0));
+			missing.add(this.get(0));
 			this.remove(0);
+		}
+	}
+	public void addCard(Card c){
+		for (int i = 0; i < missing.size(); i++){
+			if (missing.get(i).uuid() == c.uuid()){
+				this.add(c);
+				return;
+			}
 		}
 	}
 	public int valueOfHand(){
