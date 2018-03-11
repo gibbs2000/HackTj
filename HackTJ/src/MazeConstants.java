764304467d21822976edace5b001ac19be8848b7
srgib <gibbonss@bishopireton.org>
2018-03-11 09:23:58 -0400

    bigger maze

--- HackTJ/src/MazeConstants.java
+++ HackTJ/src/MazeConstants.java
@@ -5,6 +5,6 @@ public class MazeConstants {
 	public static final int WINDOW_WIDTH = 1600
 
 	;
-	public static final int DEFAULT_DIMENSIONS = 100;
+	public static final int DEFAULT_DIMENSIONS = WINDOW_HEIGHT / 15;
 
 }
